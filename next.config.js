f126adeda8ebeb5bc4945322b48f9e7c5b4ea7c4
Samuchiha12 <samyakjain@Samyaks-MacBook-Pro.local>
2023-11-06 00:23:02 +0530

    changing node version

--- next.config.js
+++ next.config.js
@@ -1,17 +1,30 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+const withLess = require("next-with-less");
+const lessToJS = require("less-vars-to-js");
+const fileSystem = require("fs");
+const path = require("path");
+
+// const themeVariables = lessToJS(
+//   fileSystem.readFileSync(
+//     path.resolve(__dirname, "./public/style/variables.less"),
+//     "utf8"
+//   )
+// );
+const ignoreCoreModules = (config) => {
+  config.resolve = {
+    ...config.resolve,
+    fallback: {
+      fs: false,
+      path: false,
+      os: false,
+    },
+  };
+  return config;
+};
+module.exports = withLess({
+  webpack: ignoreCoreModules,
+  lessLoaderOptions: {
+    lessOptions: {
+    //   modifyVars: themeVariables,
+    },
+  },
+});
